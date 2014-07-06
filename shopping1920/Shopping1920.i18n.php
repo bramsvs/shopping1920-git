6ea0059d0bd20154168de70f3121550d3ccfb925
Bram Strack van Schijndel <bramsvs@gmail.com>
2014-07-06 15:47:48 +0200

    shopping1920 init

---
+++ shopping1920/Shopping1920.i18n.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Internationalization file for skin Shopping1920.
+ *
+ * @file
+ * @ingroup Skins
+ */
+
+$messages = array();
+
+/** English
+ */
+$messages['en'] = array(
+	'skinname-shopping1920' => 'Shopping 2020',
+);
+
+/** Dutch (nederlands)
+ */
+$messages['nl'] = array(
+		'skinname-shopping1920' => 'Shopping 2020',
+);
